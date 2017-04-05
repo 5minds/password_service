e4c67e44d8bb02b1ee1973ab36a41813109d3cb8
Christoph Gnip <christoph.gnip@5minds.de>
2017-04-05 18:53:28 +0200

    :sparkles: Add the Endpoint layer

--- app/server.py
+++ app/server.py
@@ -1,15 +1,11 @@
-from flask import Flask, json, jsonify
-from flask_restful import reqparse, abort, Api
-from Service import WiFiPasswords as Service1
-from Service import Passwords as Service2
+from flask import Flask
+from flask_restful import Api
+from Endpoint import controller as controller
 
 app = Flask(__name__)
 api = Api(app)
 
-#  Router {{{ #
-api.add_resource(Service1.WiFiPasswords, '/passwords/wifi')
-api.add_resource(Service2.Passwords, '/passwords')
-#  }}} Router #
+api = controller.get_resources(api)
 
 if __name__ == '__main__':
     app.run(debug=True)
